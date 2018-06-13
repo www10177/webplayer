98d9419abeb1f93736e2d86f4a8677ff4c14cba1
Rist <www10177@gmail.com>
2018-06-14 01:43:58 +0800

    move template

--- app.py
+++ app.py
@@ -5,24 +5,22 @@ from os import remove
 from urllib.parse import unquote
 from subprocess import call
 from sys import argv
+import threading
+from random import randint
+import time
 app=Flask(__name__,static_url_path='/static',root_path='.')
 videoPath = join('./','static/','video/')
 tempPath= join('./','static/','temp/')
-# @app.route('/')
-# def index():
-    # return("Hello FUCKING World")
-#def close_clip(clip):
-#    try:
-#        clip.reader.close()
-#        del clip.reader
-#        if clip.audio != None:
- #           clip.audio.reader.close_proc()
-  #          del clip.audio
-   #     del clip
-   # except: 
-    #    print('qq')
+exporting_threads = {}
+class ExportingThread(threading.Thread):
+    def __init__(self):
+        self.progress = 0
+        super().__init__()
+    def run(self):
+        self.progress += 10
 
-def mix(basename, overlay):
+
+def createMixAudio(basename, overlay):
     basePath = join(videoPath,basename)
     base = VideoFileClip(basePath)
     music = []
@@ -31,35 +29,51 @@ def mix(basename, overlay):
         video_clip= VideoFileClip(join(videoPath,clip))
         music.append(video_clip.audio.set_duration(length))
     concat = concatenate_audioclips(music)
-    clip = CompositeAudioClip([base.audio, concat])
+    # clip = CompositeAudioClip([base.audio, concat]) # video + audio
+    clip = CompositeAudioClip([ concat]) # audio
     audioFileName=join(tempPath,basename[:-3]+'wav')
     if isfile(audioFileName):
         remove(audioFileName)
-    #audioFileName=(join(tempPath,'temp.wav'))
     print(audioFileName)
     clip.write_audiofile(audioFileName,fps=44100, nbytes=2, buffersize=2000)
-   # close_clip(base)
-   # for clip in music:
-    #    close_clip(clip)
-    tempVideoName = join(tempPath,basename)
+    return (join(tempPath,basename),basePath,audioFileName) # tempVideoName
+
+def mixVideoAudio (path):
+    tempVideoName,basePath,audioFileName = path
     if isfile(tempVideoName):
         remove(tempVideoName)
-    call(['ffmpeg', '-i', basePath, '-i', audioFileName, '-c:v', 'copy', '-c:a', 'aac', '-strict', 'experimental', '-map', '0:v:0', '-map', '1:a:0', tempVideoName])
+    call(['./ffmpeg', '-i', basePath, '-i', audioFileName, '-c:v', 'copy', '-c:a', 'aac', '-strict', 'experimental', '-map', '0:v:0', '-map', '1:a:0', tempVideoName])# combine video stream and audio
 
 
 @app.route('/')
 def video():
+    # global exporting_threads
+    # thread_id = randint(0, 10000)
+    # exporting_threads[thread_id] = ExportingThread()
+    # print ('Progress Task id: #%s' % thread_id)
     return render_template('index.html')
+@app.route('/test/')
+def run():
+    def generate():
+        yield 'waiting 5 seconds\n'
+        for i in range(1, 101):
+            time.sleep(0.05)
+            if i % 10 == 0:
+                yield '{}%\n'.format(i)
+        yield 'done\n'
 
+    return Response(generate(), mimetype='text/plain')
 
 @app.route('/result/',methods=['GET','POST'])
 def result():
     if request.method == 'GET':
         return 'QQ'
     if request.method == 'POST':
+        # exporting_threads[thread_id].start()
         video = unquote(request.values.get('video').split('/')[-1])
         overlay = ['Alan Walker - Alone.mp4','Alan Walker - Faded.mp4']
-        mix(video,overlay)
+        path = createMixAudio(video,overlay)
+        mixVideoAudio(path)
         return render_template('result.html',video=video)
 
 @app.route('/turing/',methods=['GET','POST'])
